7a8429e0132cd82e502bfa97d38bd17c329d54f4
ilyass chah <ilyasschah18@gmail.com>
2025-06-24 12:25:47 +0100

    dev 2.1

---
+++ Pages/POS/_POSDefault.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebPOS.Pages.POS
+{
+    public class _POSDefaultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
